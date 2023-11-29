8a893f6e234b1ab57edd8f3719fb1e38c4e4a1a6
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-11-29 13:39:21 +0900

    add data and model

--- app/src/main/java/com/example/affirmation/MainActivity.kt
+++ app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.affirmation
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.affirmation.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
